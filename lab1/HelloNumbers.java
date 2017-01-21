1ca5321b743c3cb7d1ad16c7e4aec43f520f14a5
Hideyoshi Takahashi <hideyoshitakahashi@MacBook-Pro.local>
2017-01-20 23:01:40 -0800

    Completed lab1

---
+++ lab1/HelloNumbers.java
@@ -0,0 +1,12 @@
+public class HelloNumbers {
+    public static void main(String[] args) {
+        int x = 0;
+        int i = 1;
+        while (i <= 10) {
+            System.out.print(x + " ");
+            x = x + i;
+            i += 1;
+        }
+        System.out.println();
+    }
+}
