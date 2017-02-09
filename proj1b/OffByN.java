ea4c19944e4d12b9e89fc63d64539002809f5372
Hideyoshi <htakahashi@berkeley.edu>
2017-02-08 21:10:39 -0800

    completed proj1b

--- proj1b/OffByN.java
+++ proj1b/OffByN.java
@@ -8,7 +8,7 @@ public class OffByN implements CharacterComparator {
     }
     @Override
     public boolean equalChars(char x, char y) {
-        if (x - y == -this.N || y - x == this.N) {
+        if (x - y == this.N || y - x == this.N) {
             return true;
         }
         return false;
