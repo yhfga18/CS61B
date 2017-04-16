80a7b790367d24de13b4356613f50b4e1c683c18
Hideyoshi <htakahashi@berkeley.edu>
2017-04-16 05:04:03 -0700

    i
    8
    
    8
    
    1

---
+++ proj3/src/main/java/QuadTree.java
@@ -0,0 +1,187 @@
+/**
+ * Created by hideyoshitakahashi on 4/14/17.
+ */
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class QuadTree {
+    public class Node implements Comparable<Node>{
+        private String filename;
+        private int depth;
+        private Map<String, Double> picScale;
+        private Node sub1, sub2, sub3, sub4;
+
+        public Node(String name, int dep) {
+            filename = name;
+            depth = dep;
+            picScale = new HashMap<String, Double>();
+//            sub1 = null;
+//            sub2 = null;
+//            sub3 = null;
+//            sub4 = null;
+
+        }
+
+        public Node(String name, int dep, Map<String, Double> scale) {
+            filename = name;
+            depth = dep;
+            picScale = scale;
+//            sub1 = null;
+//            sub2 = null;
+//            sub3 = null;
+//            sub4 = null;
+        }
+
+        public int compareTo(Node node) {
+            return Integer.parseInt(this.filename) - Integer.parseInt(node.filename);
+        }
+
+        public Map<String, Double> getPicScale() {
+            return picScale;
+        }
+
+        public String getFilename() {
+            return filename;
+        }
+
+        public int getDepth() {
+            return depth;
+        }
+
+        public Node getSub1() {
+            return sub1;
+        }
+
+        public Node getSub2() {
+            return sub2;
+        }
+
+        public Node getSub3() {
+            return sub3;
+        }
+
+        public Node getSub4() {
+            return sub4;
+        }
+    }
+
+    private Node root;
+
+    public QuadTree(Map<String, Double> scales) { //String rootName
+        root = new Node("root", 0, scales);
+        addSubsToRoot(root, "1", "2", "3", "4");
+        addSubN(1, root.sub1);
+        addSubN(1, root.sub2);
+        addSubN(1, root.sub3);
+        addSubN(1, root.sub4);
+    }
+
+    private void addSubsToRoot(Node node, String s1, String s2, String s3, String s4) {
+        node.sub1 = new Node(s1, 1);
+        node.sub2 = new Node(s2, 1);
+        node.sub3 = new Node(s3, 1);
+        node.sub4 = new Node(s4, 1);
+        scaleCalculator(node.sub1, node);
+        scaleCalculator(node.sub2, node);
+        scaleCalculator(node.sub3, node);
+        scaleCalculator(node.sub4, node);
+    }
+
+    private void addSubs(Node node, int n) {
+        node.sub1 = new Node(node.filename + "1", n);
+        node.sub2 = new Node(node.filename + "2", n);
+        node.sub3 = new Node(node.filename + "3", n);
+        node.sub4 = new Node(node.filename + "4", n);
+        scaleCalculator(node.sub1, node);
+        scaleCalculator(node.sub2, node);
+        scaleCalculator(node.sub3, node);
+        scaleCalculator(node.sub4, node);
+
+    }
+
+    private void addSubN(int n, Node p) {
+        if (n > 6) {return;}
+        addSubs(p, n);
+        addSubN(n + 1, p.sub1);
+        addSubN(n + 1, p.sub2);
+        addSubN(n + 1, p.sub3);
+        addSubN(n + 1, p.sub4);
+    }
+
+    public Map<String, Double> getNodeValues(Node node) {
+        return node.picScale;
+    }
+
+    private void scaleCalculator(Node node, Node parentNode) {
+        /*
+        if (node.filename.length() < 1 || node.filename.equals("root")) {
+            node.picScale.put("ullat", parentNode.picScale.get("ULLAT")); //upper left lati 横
+            node.picScale.put("ullon", parentNode.picScale.get("ULLON")); //upper left long 縦
+            node.picScale.put("lrlat", parentNode.picScale.get("LRLAT")); //lower right lati
+            node.picScale.put("lrlon", parentNode.picScale.get("LRLON")); //lower right long
+        }
+        */
+        //else {
+            char lastChar = node.filename.charAt(node.filename.length() - 1);
+            if (lastChar == '1') {
+                node.picScale.put("ullat", parentNode.picScale.get("ullat"));
+                node.picScale.put("ullon", parentNode.picScale.get("ullon"));
+                node.picScale.put("lrlat", parentNode.picScale.get("lrlat") / 2);
+                node.picScale.put("lrlon", parentNode.picScale.get("lrlon") / 2);
+            } else if (lastChar == '2') {
+                node.picScale.put("ullat", parentNode.picScale.get("ullat"));
+                node.picScale.put("ullon", parentNode.picScale.get("ullon") / 2);
+                node.picScale.put("lrlat", parentNode.picScale.get("lrlat") / 2);
+                node.picScale.put("lrlon", parentNode.picScale.get("lrlon"));
+            } else if (lastChar == '3') {
+                node.picScale.put("ullat", parentNode.picScale.get("ullat") / 2);
+                node.picScale.put("ullon", parentNode.picScale.get("ullon"));
+                node.picScale.put("lrlat", parentNode.picScale.get("lrlat"));
+                node.picScale.put("lrlon", parentNode.picScale.get("lrlon") / 2);
+            } else if (lastChar == '4') {
+                node.picScale.put("ullat", parentNode.picScale.get("ullat") / 2);
+                node.picScale.put("ullon", parentNode.picScale.get("ullon") / 2);
+                node.picScale.put("lrlat", parentNode.picScale.get("lrlat"));
+                node.picScale.put("lrlon", parentNode.picScale.get("lrlon"));
+            }
+        node.picScale.put("w", 256.0);
+        //}
+    }
+
+    public Node getRoot() {
+        return root;
+    }
+//    private void scaleCalcHelper(Node node, Node parentNode, ) {}
+}
+
+
+/*
+    public void addSubs(Node node, String s1, String s2, String s3, String s4) {
+        node.sub1 = new Node(s1);
+        node.sub2 = new Node(s2);
+        node.sub3 = new Node(s3);
+        node.sub4 = new Node(s4);
+    }
+
+    public void addSubs(Node node) {
+        node.sub1 = new Node(node.filename + "1");
+        node.sub2 = new Node(node.filename + "2");
+        node.sub3 = new Node(node.filename + "3");
+        node.sub4 = new Node(node.filename + "4");
+    }
+
+    public void addSubN(int n) {
+        addSubN(n, root);
+    }
+
+    public void addSubN(int n, Node p) {
+        if (n < 1) {return;}
+        addSubs(p);
+        addSubN(n-1, p.sub1);
+        addSubN(n-1, p.sub2);
+        addSubN(n-1, p.sub3);
+        addSubN(n-1, p.sub4);
+
+   }
+*/
--- proj3/src/main/java/Rasterer.java
+++ proj3/src/main/java/Rasterer.java
@@ -1,5 +1,8 @@
 import java.util.HashMap;
+import java.util.TreeMap;
 import java.util.Map;
+import java.util.List;
+import java.util.LinkedList;
 
 /**
  * This class provides all code necessary to take a query box and produce
@@ -13,9 +16,29 @@ public class Rasterer {
 
     /** imgRoot is the name of the directory containing the images.
      *  You may not actually need this for your class. */
-    public Rasterer(String imgRoot) {
+
+    QuadTree quadTree;
+    double raster_ul_lon;
+    double raster_ul_lat;
+    double raster_lr_lon;
+    double raster_lr_lat;
+    double depth;
+    boolean query_success = true;
+    List<QuadTree.Node> fileList;
+
+
+    public Rasterer(String imgRoot) { // linear
         // YOUR CODE HERE
+        Map<String, Double> rootScale = new HashMap<>();
+        fileList = new LinkedList<QuadTree.Node>();
+        rootScale.put("ullat", MapServer.ROOT_ULLAT);
+        rootScale.put("ullon", MapServer.ROOT_ULLON);
+        rootScale.put("lrlat", MapServer.ROOT_LRLAT);
+        rootScale.put("lrlon", MapServer.ROOT_LRLON);
+        rootScale.put("w", 256.0);
+        quadTree = new QuadTree(rootScale);
     }
+
 
     /**
      * Takes a user query and finds the grid of images that best matches the query. These
@@ -45,17 +68,184 @@ public class Rasterer {
      * "raster_lr_lat" -> Number, the bounding lower right latitude of the rastered image <br>
      * "depth"         -> Number, the 1-indexed quadtree depth of the nodes of the rastered image.
      *                    Can also be interpreted as the length of the numbers in the image
-     *                    string. <br>
+     *                    string. <br> // recursion の数
      * "query_success" -> Boolean, whether the query was able to successfully complete. Don't
      *                    forget to set this to true! <br>
-     * @see #REQUIRED_RASTER_REQUEST_PARAMS
+     * //@see #REQUIRED_RASTER_REQUEST_PARAMS
      */
     public Map<String, Object> getMapRaster(Map<String, Double> params) {
-        // System.out.println(params);
         Map<String, Object> results = new HashMap<>();
-        System.out.println("Since you haven't implemented getMapRaster, nothing is displayed in "
-                           + "your browser.");
+        QuadTree.Node root = quadTree.getRoot();
+        search(params, root, 1);
+        String[][] imageFiles = listToArray(fileList);
+        results.put("render_grid", imageFiles);
+        results.put("raster_ul_lon", raster_ul_lon);
+        results.put("raster_ul_lat", raster_ul_lat);
+        results.put("raster_lr_lon", raster_lr_lon);
+        results.put("raster_lr_lat", raster_lr_lat);
+        results.put("depth", depth);
+        results.put("query_success", query_success);
         return results;
     }
 
+    private void search(Map<String, Double> params, QuadTree.Node node, int d) {
+        if (node == null) {
+            return;
+        }
+        if (!(P1(params, node))) {
+            if (d == 1) {
+                query_success = false;
+            }
+            return;
+        }
+        if ((P1(params, node)) && !(P2(params, node))) {
+            if (node.getSub1() == null) {return;}
+            search(params, node.getSub1(), d + 1);
+            search(params, node.getSub2(), d + 1);
+            search(params, node.getSub3(), d + 1);
+            search(params, node.getSub4(), d + 1);
+            return;
+        }
+        if ((P1(params, node)) & P2(params, node)) {
+            fileList.add(node);
+            depth = d;
+            //depth = node.getDepth();
+            Map<String, Double> m = node.getPicScale();
+            Double ullon = m.get("ullon");
+            Double ullat = m.get("ullat");
+            Double lrlon = m.get("lrlon");
+            Double lrlat = m.get("lrlat");
+            if (raster_ul_lon < ullon) {
+                raster_ul_lon = ullon;
+            }
+            if (raster_ul_lat > ullat) {
+                raster_ul_lat = ullat;
+            }
+            if (raster_lr_lon < lrlon) {
+                raster_lr_lon = lrlon;
+            }
+            if (raster_lr_lat > lrlat) {
+                raster_lr_lat = lrlat;
+            }
+
+        }
+        return;
+    }
+
+    private boolean P1(Map<String, Double> params, QuadTree.Node node) {
+        Map<String, Double> picScale = node.getPicScale();
+        boolean cond1 = (picScale.get("ullat") > params.get("lrlat")); // lat 1
+        boolean cond2 = (picScale.get("lrlat") < params.get("ullat")); // lat 2
+        boolean cond3 = (picScale.get("lrlon") > params.get("ullon")); // lon 1
+        boolean cond4 = (picScale.get("ullon") < params.get("lrlon")); // lon 2
+        return cond1 || cond2 || cond3 || cond4;
+    }
+
+    private boolean P2(Map<String, Double> params, QuadTree.Node node) {
+        Map<String, Double> picScale = node.getPicScale();
+        double nodeLonDPP = lonDPP(picScale);
+        double rasterLonDPP = lonDPP(params);
+        return rasterLonDPP >= nodeLonDPP;
+        }
+
+    private double lonDPP(Map<String, Double> params) {
+        //double q_upperLeftlat = params.get("ullat");
+        double q_upperLeftlon = params.get("ullon");
+        //double q_lowerRightlat = params.get("lrlon");
+        double q_lowerRightlon = params.get("lrlat");
+        double q_width = params.get("w");
+        return (q_lowerRightlon - q_upperLeftlon) / q_width;
+    }
+
+    private String[][] listToArray(List<QuadTree.Node> fileList){
+        Map<Double, LinkedList<QuadTree.Node>> map = new HashMap<>();
+        for (QuadTree.Node node : fileList) {
+            Double ullat = node.getPicScale().get("ullat");
+            if (!(map.containsKey(ullat))) {
+                map.put(ullat, new LinkedList<QuadTree.Node>());
+            }
+        }
+        for (QuadTree.Node node : fileList) {
+            Double ullat = node.getPicScale().get("ullat");
+            LinkedList<QuadTree.Node> list = map.get(ullat);
+            list.add(node);
+        }
+        /*
+        for (Double key : map.keySet()) {
+            LinkedList<QuadTree.Node> list = map.get(key);
+            list.sort();
+        }
+        */
+        Double[] latitudeSet = map.keySet().toArray(new Double[1]);
+        java.util.Arrays.sort(latitudeSet);
+        String[][] returnArray = new String[latitudeSet.length][];
+        for (int i = 0; i < latitudeSet.length; i++) {
+            LinkedList<QuadTree.Node> list = map.get(latitudeSet[i]);
+            LinkedList<String> latList = new LinkedList<>();
+            for (QuadTree.Node node : list) {
+                latList.add(Double.toString(node.getPicScale().get("ullat")));
+            }
+            returnArray[i] = latList.toArray(new String[0]);
+        }
+        return returnArray;
+    }
+
+    public static void main(String[] args) {
+
+    }
+
+    /*
+    class NodeComparator implements Comparator<QuadTree.Node> {
+
+    }
+    */
 }
+
+
+
+/*
+
+    private List<QuadTree.Node> search(Map<String, Double> params, QuadTree.Node node) {
+        List<QuadTree.Node> fileList = new LinkedList<>();
+        if (node == null) {
+            return fileList;
+        }
+        if ((P1(params, node))) {
+            return fileList;
+        } else if (!(P1(params, node)) && !(P2(params, node))) {
+            if (node.getSub1() == null) {return fileList;}
+            List<QuadTree.Node> fileList1 = search(params, node.getSub1());
+            List<QuadTree.Node> fileList2 = search(params, node.getSub2());
+            List<QuadTree.Node> fileList3 = search(params, node.getSub3());
+            List<QuadTree.Node> fileList4 = search(params, node.getSub4());
+            fileList.addAll(fileList1);
+            fileList.addAll(fileList2);
+            fileList.addAll(fileList3);
+            fileList.addAll(fileList4);
+        } else if (P1(params, node) & P2(params, node)) {
+            fileList.add(node);
+            depth = node.getDepth();
+            Map<String, Double> m = node.getPicScale();
+            Double ullon = m.get("ullon");
+            Double ullat = m.get("ullat");
+            Double lrlon = m.get("lrlon");
+            Double lrlat = m.get("lrlat");
+            if (raster_ul_lon < ullon) {
+                raster_ul_lon = ullon;
+            }
+            if (raster_ul_lat > ullat) {
+                raster_ul_lat = ullat;
+            }
+            if (raster_lr_lon < lrlon) {
+                raster_lr_lon = lrlon;
+            }
+            if (raster_lr_lat > lrlat) {
+                raster_lr_lat = lrlat;
+            }
+
+        }
+        return fileList;
+    }
+
+
+*/
