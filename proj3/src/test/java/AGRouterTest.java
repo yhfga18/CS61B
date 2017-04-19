d667ae48babe34cf327163ecee59fb1aff26f871
Hideyoshi <htakahashi@berkeley.edu>
2017-04-19 11:20:02 -0700

    1 2
    3

--- proj3/src/test/java/AGRouterTest.java
+++ proj3/src/test/java/AGRouterTest.java
@@ -11,6 +11,15 @@ public class AGRouterTest extends AGMapTest {
      */
     @Test
     public void testShortestPath() throws Exception {
+        /*
+        for (TestParameters p : params) {
+            LinkedList<Long> studentRouteResult = Router.shortestPath(graph,
+                    -122.26143030962002, 37.831051606162895,
+                    -122.2650498760774, 37.83796678748061);
+            assertEquals("Found route differs for input: " + p.routeParams + ".\n",
+                    p.routeResult, studentRouteResult);
+        }
+        */
         for (TestParameters p : params) {
             LinkedList<Long> studentRouteResult = Router.shortestPath(graph,
                     p.routeParams.get("start_lon"), p.routeParams.get("start_lat"),
@@ -18,5 +27,6 @@ public class AGRouterTest extends AGMapTest {
             assertEquals("Found route differs for input: " + p.routeParams + ".\n",
                     p.routeResult, studentRouteResult);
         }
+
     }
 }
